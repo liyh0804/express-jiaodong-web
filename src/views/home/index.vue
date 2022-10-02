9695bb01564782de73b8f52906a32df4819b32ac
yahii <liyh0804@163.com>
2022-10-02 22:52:16 +0800

    add bg

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,8 +4,11 @@
 
 <style lang="less">
   .dashboard {
-    width: 7* 1920px;
-    height: 4 * 1080px;
     overflow: hidden;
+    width: 7*1920px;
+    height: 4 * 1080px;
+    background-color: #000509;
+    background: #000509 url(~@/assets/images/jz/bg.png);
+    background-size: 13440px 4320px;
   }
 </style>
