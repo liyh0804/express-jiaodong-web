d06b7460cf2c8ef3441ac05095343196b8c33f23
yahii <liyh0804@163.com>
2022-10-02 22:31:34 +0800

    resolved

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <view>aaa</view>
+  <div class="dashboard"></div>
 </template>
+
+<style lang="less">
+  .dashboard {
+    width: 7* 1920px;
+    height: 4 * 1080px;
+    overflow: hidden;
+  }
+</style>
